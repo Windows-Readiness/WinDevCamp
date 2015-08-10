e8635b201b3fa41d0af723f51fef23b11f33f9ab
AndyWigley <andy.wigley@microsoft.com>
2015-08-10 16:14:48 +0100

    Added AdaptiveUI and AdaptiveCode demos to session 1

---
+++ Presentation/01. Introduction to UWP/Demos/AdaptiveCodeCamera/AdaptiveCodeCamera/MainPage.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Windows.Media.Capture;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AdaptiveCodeCamera
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // Use a StorageFile to hold the captured image for upload.
+        StorageFile media = null;
+        MediaCapture cameraCapture;
+        bool IsCaptureInProgress;
+
+        private async Task CaptureImage()
+        {
+            // Capture a new photo or video from the device.
+            cameraCapture = new MediaCapture();
+            cameraCapture.Failed += cameraCapture_Failed;
+
+            // Initialize the camera for capture.
+            await cameraCapture.InitializeAsync();
+
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Media.Capture.CameraOptionsUI"))
+            {
+                CameraOptionsUI.Show(cameraCapture);
+            }
+            
+            captureGrid.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            previewElement.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            previewElement.Source = cameraCapture;
+            await cameraCapture.StartPreviewAsync();
+        }
+
+        private async void previewElement_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            // Block multiple taps.
+            if (!IsCaptureInProgress)
+            {
+                IsCaptureInProgress = true;
+
+                // Create the temporary storage file.
+                media = await ApplicationData.Current.LocalFolder
+                    .CreateFileAsync("capture_file.jpg", CreationCollisionOption.ReplaceExisting);
+
+                // Take the picture and store it locally as a JPEG.
+                await cameraCapture.CapturePhotoToStorageFileAsync(
+                    ImageEncodingProperties.CreateJpeg(), media);
+
+                captureButtons.Visibility = Visibility.Visible;
+
+                // Use the stored image as the preview source.
+                BitmapImage tempBitmap = new BitmapImage(new Uri(media.Path));
+                imagePreview.Source = tempBitmap;
+                imagePreview.Visibility = Visibility.Visible;
+                previewElement.Visibility = Visibility.Collapsed;
+                IsCaptureInProgress = false;
+            }
+        }
+
+        private async void cameraCapture_Failed(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
+        {
+            // It's safest to return this back onto the UI thread to show the message dialog.
+            MessageDialog dialog = new MessageDialog(errorEventArgs.Message);
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
+                async () => { await dialog.ShowAsync(); });
+        }
+
+        private async void ButtonCapture_Click(object sender, RoutedEventArgs e)
+        {
+            // Prevent multiple initializations.
+            ButtonCapture.IsEnabled = false;
+
+            await CaptureImage();
+        }
+
+        private async void ButtonRetake_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            // Reset the capture and then start again.
+            await ResetCaptureAsync();
+            await CaptureImage();
+        }
+
+        private async void ButtonCancel_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            await ResetCaptureAsync();
+        }
+
+        private async Task ResetCaptureAsync()
+        {
+            captureGrid.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            imagePreview.Visibility = Visibility.Collapsed;
+            captureButtons.Visibility = Visibility.Collapsed;
+            previewElement.Source = null;
+            ButtonCapture.IsEnabled = true;
+
+            // Make sure we stop the preview and release resources.
+            await cameraCapture.StopPreviewAsync();
+            cameraCapture.Dispose();
+            media = null;
+        }
+    }
+}
