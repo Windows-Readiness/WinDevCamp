4f03464673c0248a796794b075629887d2e378a4
AndyWigley <andy.wigley@microsoft.com>
2015-08-12 16:00:06 -0700

    Added Action Center demo

---
+++ Presentation/13. Live Tiles/Demos/ActionCenterDemo/ActionCenterDemo/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+namespace ActionCenterDemo
+{
+    sealed partial class App : Common.ApplicationBase
+    {
+        public App() : base()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override Task OnLaunchedAsync(LaunchActivatedEventArgs e)
+        {
+            // first launch navigation
+            this.NavigationService.Navigate(typeof(Views.MainPage));
+
+            return base.OnLaunchedAsync(e);
+        }
+    }
+}
