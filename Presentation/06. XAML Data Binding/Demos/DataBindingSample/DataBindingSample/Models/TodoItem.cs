7ac8ecaa4c8909dc284a902097b801d2f6c92f4b
AndyWigley <andy.wigley@microsoft.com>
2015-08-13 07:29:47 -0700

    Added Data Binding samples

---
+++ Presentation/06. XAML Data Binding/Demos/DataBindingSample/DataBindingSample/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingSample.Models
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ Presentation/06. XAML Data Binding/Demos/DataBindingSample/DataBindingSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingSample.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var item = new Models.TodoItem()
+                {
+                    Title = string.Format("Task Title {0}", i)
+                };
+                this.Items.Add(item);
+            }
+        }
+        public ObservableCollection<Models.TodoItem> Items { get; private set; }
+            = new ObservableCollection<Models.TodoItem>();
+    }
+}
